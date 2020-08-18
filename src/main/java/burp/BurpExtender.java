f4c6a33df12cc19ea12c76a7c8fcf9fa81a46965
gc <gc@gcdeMacBook-Pro.local>
2020-08-18 16:56:59 +0800

    url编码Web

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.util.List;
 
 public class BurpExtender implements IBurpExtender, IHttpListener, IProxyListener ,ITab {
     private IExtensionHelpers burpHelpers = null;
@@ -22,7 +23,7 @@ public class BurpExtender implements IBurpExtender, IHttpListener, IProxyListene
         callbacks.setExtensionName("Cryptor");
         callbacks.registerHttpListener(this);  // 注册HttpListener
         callbacks.registerProxyListener(this); // 注册ProxyListener
-        callbacks.printOutput("脚本加载成功");
+        callbacks.printOutput("脚本加载成功6");
         decryptView=new DecryptView(callbacks);
         callbacks.addSuiteTab(this);
     }
@@ -44,21 +45,59 @@ public class BurpExtender implements IBurpExtender, IHttpListener, IProxyListene
         }
     }
     private  void requestIn(IInterceptedProxyMessage message, boolean messageIsRequest) {
-        callbacks.printOutput(message.toString());
-        IHttpRequestResponse qqq=message.getMessageInfo();
-        byte[] new_requests = qqq.getRequest();
-        String req=burpHelpers.bytesToString(new_requests);
-        new_requests=burpHelpers.urlDecode(new_requests);
-        this.callbacks.printOutput(req);
-        qqq.setRequest(new_requests);
-
+        if(messageIsRequest){
+            IHttpRequestResponse qqq=message.getMessageInfo();//获取报文信息
+            byte[] new_requests = qqq.getRequest();//从豹纹对象中获取请求包
+            String req=burpHelpers.bytesToString(new_requests);//将请求转换从字节数组转换成
+            if(req.contains("jsonData=")){
+                IRequestInfo analyzeRequest=burpHelpers.analyzeRequest(message.getMessageInfo());//获取请求报文信息
+                int a=analyzeRequest.getBodyOffset();//获取请求体内容
+                String body=burpHelpers.bytesToString(new_requests).substring(a).trim();//利用substring截取豹纹体
+                String ccc=body.substring(17);//利用substring截取要解密的字段
+                String cccc=burpHelpers.urlDecode(ccc);//对解密的字段进行url转码
+                new_requests=burpHelpers.stringToBytes(cccc);//对解密的字段进行base64转码
+                String aaaa=burpHelpers.bytesToString(new_requests);//从字节转换成字符串
+                String aa = "rqId=A1&";
+                String aaa = "jsonData=";//拼接字段
+                String new_aaa = aa+aaa+aaaa;
+                new_requests=burpHelpers.stringToBytes(new_aaa);//因为请求必须是字节发送，所以要将字符串转换成字节
+                new_requests=burpHelpers.buildHttpMessage(analyzeRequest.getHeaders(), new_requests);//通过bulidhttpmessage构造请求，请求头，请求体
+//                this.callbacks.printOutput(new_aaa);//调用callbacks的日志输出
+                qqq.setRequest(new_requests);//将请求转发出去
+            }else {
+                this.callbacks.printError("未找到联盟请求，解密失败");
+            }
         }
+
+
+    }
 
 
 
     private void requestOut(IHttpRequestResponse messageInfo) {
 
+        byte[] new_requests = messageInfo.getRequest();//从豹纹对象中获取请求包
+        String req=burpHelpers.bytesToString(new_requests);//将请求转换从字节数组转换成
+        if(req.contains("jsonData=")){
+            IRequestInfo analyzeRequest=burpHelpers.analyzeRequest(messageInfo);//获取请求报文信息
+            int a=analyzeRequest.getBodyOffset();//获取请求体内容
+            String body=burpHelpers.bytesToString(new_requests).substring(a).trim();//利用substring截取豹纹体
+            String ccc=body.substring(17);//利用substring截取要解密的字段
+            String cccc=burpHelpers.urlEncode(ccc);//对解密的字段进行url转码
+            new_requests=burpHelpers.stringToBytes(cccc);//对解密的字段进行base64转码
+            String aaaa=burpHelpers.bytesToString(new_requests);//从字节转换成字符串
+            String aa = "rqId=A1&";
+            String aaa = "jsonData=";//拼接字段
+            String new_aaa = aa+aaa+aaaa;
+            new_requests=burpHelpers.stringToBytes(new_aaa);//因为请求必须是字节发送，所以要将字符串转换成字节
+            new_requests=burpHelpers.buildHttpMessage(analyzeRequest.getHeaders(), new_requests);//通过bulidhttpmessage构造请求，请求头，请求体
+            this.callbacks.printOutput(cccc);//调用callbacks的日志输出
+            messageInfo.setRequest(new_requests);//将请求转发出去
+        }else {
+            this.callbacks.printError("未加密成功");
+        }
     }
+
     private void responseIn(IHttpRequestResponse messageInfo) {
         System.out.println(messageInfo);
     }
--- src/main/java/burp/main.java
+++ src/main/java/burp/main.java
@@ -1,8 +1,161 @@
 package burp;
 
-public class main {
-    public static void main(String[] args) {
-        BurpExtender Burp=new BurpExtender();
+import java.net.URL;
+import java.util.List;
 
+public class main implements IExtensionHelpers {
+
+    public static void main(String[] args) {
+
+//        callbacks.printOutput(message.toString());
+//        IHttpRequestResponse qqq=message.getMessageInfo();
+//        byte[] new_requests = qqq.getRequest();
+//        String req=burpHelpers.bytesToString(new_requests);
+//        IRequestInfo analyzeRequest=burpHelpers.analyzeRequest(message.getMessageInfo());
+//        int a=analyzeRequest.getBodyOffset();
+//        String body=burpHelpers.bytesToString(new_requests).substring(a).trim();
+//        byte[]aaa=burpHelpers.base64Decode(body);
+//        this.callbacks.printOutput(body);
+//        qqq.setRequest(aaa);
+    }
+
+    @Override
+    public IRequestInfo analyzeRequest(IHttpRequestResponse request) {
+        return null;
+    }
+
+    @Override
+    public IRequestInfo analyzeRequest(IHttpService httpService, byte[] request) {
+        return null;
+    }
+
+    @Override
+    public IRequestInfo analyzeRequest(byte[] request) {
+        return null;
+    }
+
+    @Override
+    public IResponseInfo analyzeResponse(byte[] response) {
+        return null;
+    }
+
+    @Override
+    public IParameter getRequestParameter(byte[] request, String parameterName) {
+        return null;
+    }
+
+    @Override
+    public String urlDecode(String data) {
+        return null;
+    }
+
+    @Override
+    public String urlEncode(String data) {
+        return null;
+    }
+
+    @Override
+    public byte[] urlDecode(byte[] data) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] urlEncode(byte[] data) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] base64Decode(String data) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] base64Decode(byte[] data) {
+        return new byte[0];
+    }
+
+    @Override
+    public String base64Encode(String data) {
+        return null;
+    }
+
+    @Override
+    public String base64Encode(byte[] data) {
+        return null;
+    }
+
+    @Override
+    public byte[] stringToBytes(String data) {
+        return new byte[0];
+    }
+
+    @Override
+    public String bytesToString(byte[] data) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(byte[] data, byte[] pattern, boolean caseSensitive, int from, int to) {
+        return 0;
+    }
+
+    @Override
+    public byte[] buildHttpMessage(List<String> headers, byte[] body) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] buildHttpRequest(URL url) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] addParameter(byte[] request, IParameter parameter) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] removeParameter(byte[] request, IParameter parameter) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] updateParameter(byte[] request, IParameter parameter) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] toggleRequestMethod(byte[] request) {
+        return new byte[0];
+    }
+
+    @Override
+    public IHttpService buildHttpService(String host, int port, String protocol) {
+        return null;
+    }
+
+    @Override
+    public IHttpService buildHttpService(String host, int port, boolean useHttps) {
+        return null;
+    }
+
+    @Override
+    public IParameter buildParameter(String name, String value, byte type) {
+        return null;
+    }
+
+    @Override
+    public IScannerInsertionPoint makeScannerInsertionPoint(String insertionPointName, byte[] baseRequest, int from, int to) {
+        return null;
+    }
+
+    @Override
+    public IResponseVariations analyzeResponseVariations(byte[]... responses) {
+        return null;
+    }
+
+    @Override
+    public IResponseKeywords analyzeResponseKeywords(List<String> keywords, byte[]... responses) {
+        return null;
     }
 }
